8757fa3aa0540432b8ea3c59a2dc5a92cd51cac0
huang <huang.jingquan@163.com>
2015-07-30 16:32:21 +0800

    validate something

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+	# 检测字段存在且不为空
+	validates_presence_of :title, :description, :image_url
+	validates_numericality_of :price
+	validate :price_must_be_at_least_a_cent
+
+	protected
+	def price_must_be_at_least_a_cent
+		errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
+	end
 end
--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730072059) do
+ActiveRecord::Schema.define(version: 20150730080729) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150730072059) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price"
   end
 
 end
