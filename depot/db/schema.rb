bcf706a62a3b17a6e4422241349bbc495662d310
huang <huang.jingquan@163.com>
2015-07-30 17:22:19 +0800

    layout

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730080729) do
+ActiveRecord::Schema.define(version: 20150730083559) do
 
   create_table "products", force: true do |t|
     t.string   "title"
